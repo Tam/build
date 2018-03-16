b9bd6580eb352abc9a8a080913d36166885ce797
Tam <hi@tam.sx>
2018-03-16 13:48:10 +0000

    fixed external helpers bug

--- bin/build/js.js
+++ bin/build/js.js
@@ -100,7 +100,6 @@ const compiler = createCompiler("js", webpack, {
 								]
 							],
 							plugins: [
-								require.resolve("babel-plugin-external-helpers"),
 								require.resolve("babel-plugin-transform-class-properties"),
 								require.resolve("babel-plugin-transform-object-rest-spread"),
 							],
