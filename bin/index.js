f4ce08205e8a61cf0db773c93992f60535030dbb
Tam <Tam@users.noreply.github.com>
2018-08-14 14:49:39 +0100

    Fixed a bug where `once` was setting NODE_ENV too late

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,15 @@
 #! /usr/bin/env node
 
+const args = process.argv.slice(2);
+
+if (args[0] === "once")
+	process.env.NODE_ENV = "production";
+
 const build = require("./build");
 
 // require("./update");
 
 !async function () {
-	const args = process.argv.slice(2);
-	
 	switch (args[0]) {
 		case "init":
 			require("./init");
