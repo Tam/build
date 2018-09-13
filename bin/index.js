7d208f1d99cd2a937256dbb441b3402667e80a52
Tam <hi@tam.sx>
2018-09-13 22:09:14 +0100

    Fresh start / init

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,14 @@
 #! /usr/bin/env node
 
-const args = process.argv.slice(2);
-
-if (args[0] === "once")
-	process.env.NODE_ENV = "production";
-
-const build = require("./build");
-
-// require("./update");
-
 !async function () {
-	switch (args[0]) {
+	switch (process.argv[2]) {
 		case "init":
 			require("./init");
 			return;
 		case "once":
-			process.env.NODE_ENV = "production";
-			await build(true);
-			process.exit();
+			// TODO: Production build
 			return;
 		default:
-			await build();
+			// TODO: Development build / watch
 	}
 }();
--- bin/init.js
+++ bin/init.js
@@ -1,13 +1,29 @@
-const config = require("./const").config
-	, fs = require("fs")
-	, chalk = require("chalk");
+const chalk = require("chalk")
+	, inquirer = require("inquirer")
+	, fs = require("fs");
 
-delete config.__isDefault;
+!async function () {
 
-/**
- * Create the .buildrc file using the default config
- */
-fs.writeFile(".buildrc", JSON.stringify(config, null, "\t"), (err) => {
-	if (err) throw err;
-	console.log(chalk.bold.green("The config file has been saved!"));
-});
+	if (fs.existsSync("build.config.js")) {
+		const { overwrite } = await inquirer.prompt([{
+			type: "confirm",
+			name: "overwrite",
+			message: "build.config.js already exists, overwrite?",
+			default: false,
+		}]);
+
+		if (!overwrite)
+			return;
+	}
+
+	const defaultConfig = fs.readFileSync(
+		__dirname + "/default.config.js",
+		"utf8"
+	);
+
+	fs.writeFile("build.config.js", defaultConfig, err => {
+		if (err) throw err;
+		console.log(chalk.bold.green("The config file has been saved!"));
+	});
+
+}();
