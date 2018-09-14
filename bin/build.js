1394d4eeb96bf332cbe2d05a8f0c7466923efc4b
Tam <Tam@users.noreply.github.com>
2018-09-14 09:18:40 +0100

    Update build.js

--- bin/build.js
+++ bin/build.js
@@ -3,3 +3,4 @@ const config = require("./config");
 console.log(config);
 
 // Less watch depencencies only: https://github.com/JakeSidSmith/watch-less-do-more
+// Or w/ the parser only: https://gist.github.com/Craga89/96f7fcad1f35b08bf88d#file-lessimportlist-js
